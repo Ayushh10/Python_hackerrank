86009cd2f12696e7f557565302f20d83c495f2a9
Ayush <43788474+Ayushh10@users.noreply.github.com>
2020-06-30 11:48:45 +0530

    Updated 'Capitalize()'

--- Capitalize.py
+++ Capitalize.py
@@ -1,5 +1,17 @@
 def solve(s):
-    first_half, second_half = s.split(" ")
-    first_letter, second_letter = first_half[0], second_half[0]
-    first_rem, second_rem = first_half[1:], second_half[1:]
-    return first_letter.capitalize()+first_rem+" "+second_letter.capitalize()+second_rem
+    for i in s[:].split():
+        s = s.replace(i, i.capitalize())
+    return s
+   
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
